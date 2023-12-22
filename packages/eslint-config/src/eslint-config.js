babf6033708143061e3420a950f4b07ef1d56ff2
bruno palma <bruno.palma@xempus.com>
2023-12-22 21:25:41 +0100

    sm0l changes

---
+++ packages/eslint-config/src/eslint-config.js
@@ -0,0 +1,27 @@
+"use strict";
+exports.__esModule = true;
+var config = {
+    parser: '@typescript-eslint/parser',
+    globals: {
+        MyGlobal: true
+    },
+    plugins: ['@typescript-eslint', 'sonarjs'],
+    "extends": [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:playwright/playwright-test',
+        'plugin:prettier/recommended',
+        'plugin:unicorn/recommended',
+        'plugin:sonarjs/recommended',
+    ],
+    rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
+        'sonarjs/cognitive-complexity': 'error',
+        'sonarjs/no-identical-expressions': 'error'
+    },
+    parserOptions: {
+        project: './tsconfig.json'
+    }
+};
+exports["default"] = config;
